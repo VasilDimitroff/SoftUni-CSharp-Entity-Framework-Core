d88c9a9d6e1133a4a049a8508499486843811aa1
Vasil Dimitrov <v.b.dimitrow@gmai.com>
2021-03-04 00:12:31 +0200

    change

--- JsonProcessing/ProductShop/ProductShop/StartUp.cs
+++ JsonProcessing/ProductShop/ProductShop/StartUp.cs
@@ -11,7 +11,53 @@ namespace ProductShop
     public class StartUp
     {
         public static void Main(string[] args)
+        {   
+            var context = new ProductShopContext();
+
+            context.Database.EnsureCreated();
+
+            string json = File.ReadAllText("../../../Datasets/categories.json");
+            var result = ImportCategories(context, json);
+            Console.WriteLine(result);
+        }
+
+        //Problem 1
+        public static string ImportUsers(ProductShopContext context, string inputJson)
         {
+            User[] users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        //Problem 2
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            Product[] products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.Products.AddRange(products);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+        //Problem 3
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            Category[] categories = JsonConvert.DeserializeObject<Category[]>(inputJson)
+                .Where(x => x.Name != null)
+                .ToArray();
+
+            context.Categories.AddRange(categories);
+
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Length}";
 
         }
     }
